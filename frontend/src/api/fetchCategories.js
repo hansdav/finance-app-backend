bebe1dd4ae9cd8de2e9ea9e41cd98652fc41ac4e
Hans Davidson <103026522+hansdav@users.noreply.github.com>
2023-03-11 12:05:48 +0100

    Delete frontend directory

--- frontend/src/api/fetchCategories.js
+++
@@ -1,9 +0,0 @@
-const fetchCategories = async (parameters) => {
-    let baseUrl = "http://localhost:3000/api/categories"
-    const res = await fetch(baseUrl);
-    let data = await res.json();
-
-  return data;
-}
-
-export default fetchCategories
